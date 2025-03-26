e3cc14e3b7b1d07de14a7ad598ce9f77631763da
FelixNavratil <94867571+FelixNavratil@users.noreply.github.com>
2025-03-26 18:53:20 +0100

    Delete RP3_2D_simulace_zvuku_v_mistnosti directory

--- RP3_2D_simulace_zvuku_v_mistnosti/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.rp3_2d_simulace_zvuku_v_mistnosti {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.desktop;
-
-
-    opens com.example.rp3_2d_simulace_zvuku_v_mistnosti to javafx.fxml;
-    exports com.example.rp3_2d_simulace_zvuku_v_mistnosti;
-}
